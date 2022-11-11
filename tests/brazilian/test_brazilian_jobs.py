2f41813acac5aa1dfd60ff7c4997181a4b2cd2c5
Istvan <olivaistvan@gmail.com>
2022-11-10 22:46:44 -0300

    test: test_brazilian_jobs

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,6 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    brazilian_file = read_brazilian_file('tests/mocks/brazilians_jobs.csv')
+    assert brazilian_file[0].get('title')
